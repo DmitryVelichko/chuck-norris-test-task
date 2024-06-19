38b14852cd8d6084a1d383c415b75baa4e63ab66
Dmitry Velichko <soleil-2005@mail.ru>
2024-06-19 18:54:25 +0300

    Add MVP + small fixes

--- components/SearchJokes.js
+++ components/SearchJokes.js
@@ -9,21 +9,21 @@ export default function SearchJokes() {
     const [jokes, setJokes] = useState([]);
     const [prevQueries, setPrevQueries] = useState(new Set());
     const [totalJokes, setTotalJokes] = useState("0")
-    const [loading, setLoading] = useState(false); 
+    const [loading, setLoading] = useState(false);
 
 
 
     function handleSearch() {
-        
-        if(query.length < 4) {
+
+        if (query.length < 4) {
             alert("Мало символов для запроса! Нужно минимум 4.")
         }
 
-        if(prevQueries.has(query)) {
+        if (prevQueries.has(query)) {
             alert(`Запрос "${query}" уже был. Введите новый запрос или обновите страницу.`)
         }
 
-        
+
 
         if (query.length >= 4 && !prevQueries.has(query)) {
             setLoading(true);
@@ -35,8 +35,8 @@ export default function SearchJokes() {
                     setLoading(false);
                 })
                 .catch(error => console.error('Error fetching jokes:', error));
-                
-               
+
+
         }
     }
 
@@ -64,7 +64,7 @@ export default function SearchJokes() {
             <p className={styles.totalCount}>Всего шуток: {totalJokes}</p>
             {loading && <div className={styles.spinner}>Loading...</div>}
             <div className={styles.jokesContainer}>
-            {totalJokes === 0 ? <div className={styles.noJokesFound}>Шуток не найдено! </div> : null}
+                {totalJokes === 0 ? <div className={styles.noJokesFound}>Шуток не найдено! </div> : null}
                 {jokes.map(joke => (
                     <div key={joke.id} className={styles.jokeCard}>
                         {joke.value}
--- features/searchSlice.js
+++ features/searchSlice.js
@@ -1,23 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const searchSlice = createSlice({
-  name: 'search',
-  initialState: {
-    query: '',
-    jokes: [],
-    totalCount: 0,
-  },
-  reducers: {
-    setQuery: (state, action) => {
-      state.query = action.payload;
+    name: 'search',
+    initialState: {
+        query: '',
+        jokes: [],
+        totalCount: 0,
     },
-    setJokes: (state, action) => {
-      state.jokes = action.payload;
+    reducers: {
+        setQuery: (state, action) => {
+            state.query = action.payload;
+        },
+        setJokes: (state, action) => {
+            state.jokes = action.payload;
+        },
+        setTotalCount: (state, action) => {
+            state.totalCount = action.payload;
+        },
     },
-    setTotalCount: (state, action) => {
-      state.totalCount = action.payload;
-    },
-  },
 });
 
 export const { setQuery, setJokes, setTotalCount } = searchSlice.actions;
